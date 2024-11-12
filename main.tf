9e09f1916101af83ecfe8e241c027e38935963bc
sandeepsf944 <154037075+sandeepsf944@users.noreply.github.com>
2024-11-13 02:31:26 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,36 +1,37 @@
 module "resourcegroup" {
-  source             = "../modules/resourcegrop"
-  rg1_name = "testrg1"
+  source   = "../modules/resourcegrop"
+  rg1_name = "testrg"
   location = "East US 2"
 }
 
 
 module "appserviceplan" {
-  source             = "../modules/appserviceplan"
-  location = module.resourcegrop.location
-  linux_web_app_name = "sandeepsf94gskapp" 
-  service_plan_name = "sandeepsf94gskASP"
-  sku_name = "B3"
-  deployed_by = "sandeep"
+  source              = "../modules/appserviceplan"
+  location            = module.resourcegroup.location
+  resource_group_name = module.resourcegroup.resource_group_name
+  linux_web_app_name  = "sandeepsf94gskapp"
+  service_plan_name   = "sandeepsf94gskASP"
+  sku_name            = "B3"
+  deployed_by         = "sandeep"
 }
 
-#   vpc_name      = module.alpha_vpc_1.vpc_name
-#   vpc_id        = module.alpha_vpc_1.vpc_id
-#   environment   = module.alpha_vpc_1.environment
+# #   vpc_name      = module.alpha_vpc_1.vpc_name
+# #   vpc_id        = module.alpha_vpc_1.vpc_id
+# #   environment   = module.alpha_vpc_1.environment
 
 module "linuxwebapp" {
-  source             = "../modules/linuxwebapp"
-  linux_web_app_name = "sandeepsf94gskapp"
-  app_startup_command = "gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 main:app"
-  ftps_state = "FtpsOnly"
+  source                            = "../modules/linuxwebapp"
+  linux_web_app_name                = module.appserviceplan.linux_web_app_name
+  service_plan_id                   = module.appserviceplan.service_plan_id
+  app_startup_command               = "gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 main:app"
+  ftps_state                        = "FtpsOnly"
+  location                          = module.resourcegroup.location
+  resource_group_name               = module.resourcegroup.resource_group_name
   scm_ip_restriction_default_action = "Allow"
-  ip_restriction_default_action = "Allow"
-  ip_restriction_list = ["10.1.0.0/16", "192.2.20.0/32"]
-  restricted_ip_name_list = ["test1", "test2"]
-  python_version = 3.9
-  zip_deploy_file_path = "/msdocs-python-fastapi-webapp-quickstart.zip"
-}  
-
-
-
-
+  ip_restriction_default_action     = "Allow"
+  new_ip_restriction_list               = ["10.1.0.0/16", "192.2.20.0/32"]
+  restricted_ip_name_list           = ["test1", "test2"]
+  python_version                    = 3.9
+  webdeploy_publish_basic_authentication_enabled = false
+  zip_deploy_file_path              = "./msdocs-python-fastapi-webapp-quickstart.zip"
+}
